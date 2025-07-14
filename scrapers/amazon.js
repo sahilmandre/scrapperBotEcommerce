d970f012b3a11c0d3f44dfaf252a88ec6f5342cf
Sahil Mandre <sahilmandre@gmail.com>
2025-07-15 01:49:05 +0530

    Both the amazon and flipkart extractions working fine

--- scrapers/amazon.js
+++ scrapers/amazon.js
@@ -6,50 +6,49 @@ import { urls } from "../config/urls.js";
 
 export async function scrapeAmazon() {
   const allProducts = [];
+  for (const { url, type, platform } of urls.filter(
+    (u) => u.platform === "amazon"
+  )) {
+    console.log("🔍 Scraping:", url);
 
-  for (const url of urls) {
-    try {
-      console.log("🔍 Scraping:", url);
-      const { data: html } = await axios.get(url, {
-        headers: {
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-          "Accept-Language": "en-US,en;q=0.9",
-        },
-      });
+    const { data: html } = await axios.get(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept-Language": "en-US,en;q=0.9",
+      },
+    });
 
-      const $ = cheerio.load(html);
+    const $ = cheerio.load(html);
 
-      let products = [];
+    const products = [];
 
-      $("div[data-asin]:has(h2)").each((_, el) => {
-        const title = $(el).find("h2 span").text().trim();
-        const href = $(el).find("a.a-link-normal").attr("href");
-        const link = href?.startsWith("http")
-          ? href
-          : `https://www.amazon.in${href}`;
-        const price = $(el).find(".a-price .a-offscreen").first().text().trim();
-        const mrp = $(el)
-          .find(".a-price.a-text-price .a-offscreen")
-          .first()
-          .text()
-          .trim();
+    $("div[data-asin]:has(h2)").each((_, el) => {
+      const title = $(el).find("h2 span").text().trim();
+      const href = $(el).find("a.a-link-normal").attr("href");
+      const link = href?.startsWith("http")
+        ? href
+        : `https://www.amazon.in${href}`;
+      const price = $(el).find(".a-price .a-offscreen").first().text().trim();
+      const mrp = $(el)
+        .find(".a-price.a-text-price .a-offscreen")
+        .first()
+        .text()
+        .trim();
 
-        if (title && price && link) {
-          products.push({
-            platform: "amazon",
-            title,
-            link,
-            price,
-            mrp,
-          });
-        }
-      });
+      if (title && price && link) {
+        products.push({
+          platform,
+          type,
+          title,
+          price,
+          mrp,
+          link,
+        });
+      }
+    });
 
-      console.log(`✅ Found ${products.length} products from: ${url}`);
-      allProducts.push(...products);
-    } catch (err) {
-      console.error("❌ Amazon Scrape failed for", url, "-", err.message);
-    }
+    console.log(`✅ Found ${products.length} products from: ${url}`);
+    allProducts.push(...products);
   }
 
   return allProducts;
